d2377f13079d90a67055cfc9681cbbd3dfd8e65d
Santiago Cortés <109761550+scortesg1@users.noreply.github.com>
2023-08-02 16:32:31 -0500

    Create arrowVSRegular.js

---
+++ arrowVSRegular.js
@@ -0,0 +1,7 @@
+const isEvenRegular = function(number) {
+    return number % 2 == 0 ? 'Es par' : 'No es par' 
+}
+
+const isEvenArrow = (number) => {
+    return number % 2 == 0 ? 'Es par' : 'No es par' 
+}
