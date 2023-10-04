f8fbd300f6c525ecc1c7244f2435feba8d2d653e
Santiago Cortés <109761550+scortesg1@users.noreply.github.com>
2023-10-04 16:08:37 -0500

    a

---
+++ first-app/src/FirstApp.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function FirstApp() {
+
+  return (
+    <>
+     <h1>Counter</h1>
+     <span>10</span>
+    </>
+  )
+}
+
+export default FirstApp
