c38c82d8abe90f6f0a1af1e0df4abeca99c6238e
Santiago Cortés <109761550+scortesg1@users.noreply.github.com>
2023-10-04 16:13:20 -0500

    Breve

--- first-app/src/FirstApp.jsx
+++ first-app/src/FirstApp.jsx
@@ -5,10 +5,26 @@ import './App.css'
 
 function FirstApp() {
 
+  const [counter, setCounter] = useState(0)
+
+  const handleAdd = () => {
+    setCounter(counter + 1)
+  }
+  const handleSubstract = () => {
+    setCounter(counter - 1)
+  }
+  const handleReset = () => {
+    setCounter(0)
+  }
+
   return (
     <>
      <h1>Counter</h1>
-     <span>10</span>
+     <span>{counter}</span>
+     <button onClick={() => handleAdd()}>+1</button>
+     <button onClick={() => handleSubstract()}>-1</button>
+     <button onClick={() => handleReset()}>Reset</button>
+
     </>
   )
 }
