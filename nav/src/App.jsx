8364b2bdb4e95f48ba0ccbef8772f8f33d79eaba
Santiago Cortés <109761550+scortesg1@users.noreply.github.com>
2023-11-22 18:50:32 -0500

    cosas y mas cosas

---
+++ nav/src/App.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useReducer } from "react";
+import NavComponent from "./components/NavComponent";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Experience from "./pages/Experience";
+import Portfolio from "./pages/Portfolio";
+import AboutMe from "./pages/AboutMe";
+import Home from "./pages/Home";
+function App() {
+  
+  return (
+    <>
+      <NavComponent></NavComponent>
+      <Routes >
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/experience" element={<Experience/>}></Route>
+        <Route path="/portfolio" element={<Portfolio/>}></Route>
+        <Route path="/about-me" element={<AboutMe/>}></Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ nav/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <main>
+      Página principal, escoja en el menú una opción para navegar a otras
+      páginas
+    </main>
+  );
+}
+
+export default Home;
